21bb33b8152f97cbb036caec2309ef0efa687217
Busra <Busra@DESKTOP-VU218T0>
2024-04-06 23:31:09 +0300

    Proje dosyası ekle.

---
+++ firstProject/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace firstProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.mailTextbox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.sonucLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Wide Latin", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(27, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Giriş Formu";
+            // 
+            // mailTextbox
+            // 
+            this.mailTextbox.Location = new System.Drawing.Point(31, 61);
+            this.mailTextbox.Name = "mailTextbox";
+            this.mailTextbox.Size = new System.Drawing.Size(531, 22);
+            this.mailTextbox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Wide Latin", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(31, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(531, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Giriş Yap";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Violet;
+            this.panel1.Controls.Add(this.sonucLabel);
+            this.panel1.Location = new System.Drawing.Point(31, 139);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(531, 100);
+            this.panel1.TabIndex = 3;
+            // 
+            // sonucLabel
+            // 
+            this.sonucLabel.AutoSize = true;
+            this.sonucLabel.Font = new System.Drawing.Font("Cooper Black", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sonucLabel.Location = new System.Drawing.Point(232, 42);
+            this.sonucLabel.Name = "sonucLabel";
+            this.sonucLabel.Size = new System.Drawing.Size(0, 20);
+            this.sonucLabel.TabIndex = 0;
+            this.sonucLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Purple;
+            this.ClientSize = new System.Drawing.Size(607, 328);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.mailTextbox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox mailTextbox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label sonucLabel;
+    }
+}
+
